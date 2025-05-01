a4372fd79eb5f7fb9d59557517b25f2f21b18fbc
Giacomo Amoroso <giacomo@amoroso.me>
2025-05-01 21:38:53 +0200

    Completed CH02

---
+++ shift/shift.go
@@ -0,0 +1,10 @@
+package shift
+
+func Encipher(plaintext []byte, key byte) (ciphertext []byte) {
+  ciphertext = make([]byte, len(plaintext))
+  for i, b := range plaintext {
+    ciphertext[i] = b + key 
+  }
+  return ciphertext
+}
+
