49a8157efa3ebd27120316d5bbe62520ae98958d
Giacomo Amoroso <giacomo@amoroso.me>
2025-05-02 23:22:19 +0200

    Completed CH03

--- shift/shift_test.go
+++ shift/shift_test.go
@@ -8,49 +8,65 @@ import (
 	"github.com/dankski/shift"
 )
 
-func TestEncipherTransforms(t *testing.T) {
+var cases = []struct {
+	key                   byte
+	plaintext, ciphertext []byte
+}{
+	{
+		key:        1,
+		plaintext:  []byte("HAL"),
+		ciphertext: []byte("IBM"),
+	},
+	{
+		key:        2,
+		plaintext:  []byte("SPEC"),
+		ciphertext: []byte("URGE"),
+	},
+	{
+		key:        3,
+		plaintext:  []byte("PERK"),
+		ciphertext: []byte("SHUN"),
+	},
+	{
+		key:        4,
+		plaintext:  []byte("GEL"),
+		ciphertext: []byte("KIP"),
+	},
+	{
+		key:        7,
+		plaintext:  []byte("CHEER"),
+		ciphertext: []byte("JOLLY"),
+	},
+	{
+		key:        10,
+		plaintext:  []byte("BEEF"),
+		ciphertext: []byte("LOOP"),
+	},
+}
+
+func TestEncipher(t *testing.T) {
 	t.Parallel()
-	tcs := []struct {
-		key         byte
-		input, want []byte
-	}{
-		{
-			key:   1,
-			input: []byte("HAL"),
-			want:  []byte("IBM"),
-		},
-		{
-			key:   2,
-			input: []byte("SPEC"),
-			want:  []byte("URGE"),
-		},
-		{
-			key:   3,
-			input: []byte("PERK"),
-			want:  []byte("SHUN"),
-		},
-		{
-			key:   4,
-			input: []byte("GEL"),
-			want:  []byte("KIP"),
-		},
-		{
-			key:   7,
-			input: []byte("CHEER"),
-			want:  []byte("JOLLY"),
-		},
-		{
-			key:   10,
-			input: []byte("BEEF"),
-			want:  []byte("LOOP"),
-		},
-	}
-	for _, tc := range tcs {
-		name := fmt.Sprintf("%s + %d =  %s", tc.input, tc.key, tc.want)
+
+	for _, tc := range cases {
+		name := fmt.Sprintf("%s + %d =  %s", tc.plaintext, tc.key, tc.ciphertext)
 		t.Run(name, func(t *testing.T) {
-			got := shift.Encipher(tc.input, tc.key)
-			if !bytes.Equal(tc.want, got) {
-				t.Errorf("want %q, got %q", tc.want, got)
+			got := shift.Encipher(tc.plaintext, tc.key)
+			if !bytes.Equal(tc.ciphertext, got) {
+				t.Errorf("want %q, got %q", tc.ciphertext, got)
+			}
+		})
+	}
+}
+
+func TestDecipherWithKeyTransformsIBMToHal(t *testing.T) {
+	t.Parallel()
+	for _, tc := range cases {
+		name := fmt.Sprintf("%s - %d = %s", tc.ciphertext, tc.key,
+			tc.plaintext)
+		t.Run(name, func(t *testing.T) {
+			got := shift.Decipher(tc.ciphertext, tc.key)
+			if !bytes.Equal(tc.plaintext, got) {
+				t.Errorf("want %q, got %q", tc.plaintext, got)
 			}
 		})
 	}
