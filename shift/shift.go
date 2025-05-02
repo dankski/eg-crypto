8c152e1878acef1fd656ab12f78595f69e58b0ca
Giacomo Amoroso <giacomo@amoroso.me>
2025-05-02 23:22:19 +0200

    Completed CH03

--- shift/shift.go
+++ shift/shift.go
@@ -1,10 +1,18 @@
 package shift
 
 func Encipher(plaintext []byte, key byte) (ciphertext []byte) {
-  ciphertext = make([]byte, len(plaintext))
-  for i, b := range plaintext {
-    ciphertext[i] = b + key 
-  }
-  return ciphertext
+	ciphertext = make([]byte, len(plaintext))
+	for i, b := range plaintext {
+		ciphertext[i] = b + key
+	}
+	return ciphertext
 }
 
+func Decipher(ciphertext []byte, key byte) (plaintext []byte) {
+	plaintext = make([]byte, len(ciphertext))
+
+	for i, b := range ciphertext {
+		plaintext[i] = b - key
+	}
+	return plaintext
+}
