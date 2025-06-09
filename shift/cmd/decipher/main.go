7acc55251c4036ad5b022061004d48aa6cffb283
Giacomo Amoroso <giacomo@amoroso.me>
2025-06-09 16:16:52 +0200

    CH06 add hex string parsing for key values

--- shift/cmd/decipher/main.go
+++ shift/cmd/decipher/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -10,13 +11,20 @@ import (
 )
 
 func main() {
-	key := flag.Int("key", 1, "shift value")
+	keyHex := flag.String("key", "01", "key in hexadecimal (for example 'FF'")
 	flag.Parse()
+
+	key, err := hex.DecodeString(*keyHex)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	ciphertext, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	plaintext := shift.Decipher(ciphertext, byte(*key))
+	plaintext := shift.Decipher(ciphertext, key)
 	os.Stdout.Write(plaintext)
 }
